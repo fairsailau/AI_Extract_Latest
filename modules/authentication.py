914f9a55e9b8e9603c86606e391736a1595e0d0c
fairsailau <fairsailau@gmail.com>
2025-05-06 08:57:02 +1000

    Update authentication.py

--- modules/authentication.py
+++ modules/authentication.py
@@ -8,7 +8,7 @@ import logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, 
-                   format=\'%(asctime)s - %(name)s - %(levelname)s - %(message)s\')
+                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # --- Helper function to check for secrets ---
